3d69c0c9a6b4cfe681315628526e032af3b25120
SebasDh56 <amable.quishpe@udla.edu.ec>
2024-09-30 11:18:37 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AQweb1_30_sep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
